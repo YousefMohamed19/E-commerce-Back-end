65674f110986e34a60aede47835da8872281d5b1
YousefMohamed19 <yousef01121977646@gmail.com>
2024-08-25 01:45:53 +0300

    handle error

--- src/utils/webhook.js
+++ src/utils/webhook.js
@@ -2,25 +2,20 @@ import Stripe from "stripe";
 import { Cart, Order, Product } from "../../db/index.js";
 import { asyncHandler } from "./asyncHandler.js";
 
-export const webhook = asyncHandler(async (req, res) => {
-    const sig = req.headers['stripe-signature'].toString();
-    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
-    let event = Stripe.webhooks.constructEvent(req.body, sig, 'whsec_KpOJGP7USpDiZYdGtkqHUMSgABqPz9rj');
+export const webhook = asyncHandler(
+    async (req, res) => {
+        const sig = req.headers['stripe-signature'].toString()
+        const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+        let event = stripe.webhooks.constructEvent(req.body, sig, 'whsec_9tc9ABjo2hhKEk0yIgUWdvW8KW0Jkx70');
 
-    if (event.type == 'checkout.session.completed') {
-        const checkout = event.data.object;
-        const orderId = checkout.metadata.orderId;
-        const orderExist = await Order.findByIdAndUpdate(orderId, { status: 'placed' }, { new: true })
-        await Cart.findOneAndUpdate({ user: orderExist.user }, { products: [] }, { new: true })
-        for (const product of orderExist.products) {
-            await Product.findByIdAndUpdate(product.productId, { $inc: { stock: -product.quantity } })
+
+        // Handle the event checkout.session.completed
+        if (event.type === 'checkout.session.completed') {
+            const checkout = event.data.object
+            const orderId = checkout.metadata.orderId
+            const orderExist = await Order.findByIdAndUpdate(orderId, { status: 'placed' }, { new: true })
+            const cart = await Cart.findOneAndUpdate({ user: orderExist.user }, { products: [] } , { new: true })
         }
-        // clear cart
-        // update order status placed
-        // product stock
-    }
-
-
-    // Return a 200 res to acknowledge receipt of the event
-    res.send();
-})
+        // return a 200 res to acknowledge receipt of the event
+        res.send()
+    })
